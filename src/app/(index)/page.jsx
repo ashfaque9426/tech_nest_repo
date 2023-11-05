be997b0c4317464f097dc47b01d2b2569bdec707
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-11-05 19:54:15 +0600

    Necessary npm packages installed as dependencies.

--- src/app/(index)/page.jsx
+++ src/app/(index)/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Page() {
   return (
     <main role='main'>
-        <h1 className='text-2xl font-bold text-center'>There will be home page content</h1>
+      <h1 className='text-2xl font-bold text-center'>There will be home page content</h1>
     </main>
   )
 }
