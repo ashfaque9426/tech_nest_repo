dd497c1f09a869736c6917c3673d6b0f9943f3fc
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-11-05 20:01:04 +0600

    some components modified for beauty which will be touched later

--- src/app/(index)/components/client/shared/Navbar/Navbar.jsx
+++ src/app/(index)/components/client/shared/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav>Navbar</nav>
+    <nav className='mb-5 flex justify-center items-center border py-3'>
+      <h1 className='text-xl text-center font-bold'>Tech Nest Navbar</h1>
+    </nav>
   )
 }
 
