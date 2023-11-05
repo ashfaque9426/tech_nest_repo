4d911e99667e88ce4bd265353592ee9c64c6f99c
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-11-05 18:38:16 +0600

    shell setup for Tech-Nest is done using next.js

---
+++ src/app/(index)/layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/client/shared/Navbar/Navbar'
+import Footer from './components/server/shared/Footer/Footer'
+
+function Layout1({children}) {
+  return (
+    <>
+        <Navbar />
+        {children}
+        <Footer />
+    </>
+  )
+}
+
+export default Layout1
---
+++ src/app/(index)/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <main role='main'>
+        <h1 className='text-2xl font-bold text-center'>There will be home page content</h1>
+    </main>
+  )
+}
+
+export default Page
