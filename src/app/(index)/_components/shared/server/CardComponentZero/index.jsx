2c27b39c316598fd91196b7f408f448ff989eb6f
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-12-15 03:24:25 +0600

    some styles modified for card element.

--- src/app/(index)/_components/shared/server/CardComponentZero/index.jsx
+++ src/app/(index)/_components/shared/server/CardComponentZero/index.jsx
@@ -7,26 +7,26 @@ import Link from 'next/link';
 function CardComponentZero({ id, className, imgSrcUrl, altTextForImg, cardTitle, status, price, offer, date, points }) {
   return (
     <Link href="">
-      <article className={cn('overflow-clip shadow-md h-full flex flex-col cursor-pointer', className)}>
+      <article className={cn('overflow-clip shadow-md hover:shadow-lg transition-all duration-150 h-full flex flex-col cursor-pointer group', className)}>
         <figure className='relative overflow-clip'>
           {/* offers section */}
           {
-            offer.length > 0 && <span className='absolute top-2 right-2 bg-red-500 text-white font-bold border border-red-500 rounded-xl px-3'>{offer.length > 0 && offer}</span>
+            offer.length > 0 && <span className='absolute z-10 top-2 right-2 bg-red-500 text-white font-bold border border-red-500 rounded-xl px-3'>{offer.length > 0 && offer}</span>
           }
 
           {/* points section */}
           {
-            points > 0 && <span className={`absolute ${offer.length > 0 ? 'top-10' : 'top-2'} right-2 bg-purple-900 px-3 rounded-xl font-bold text-white`}>Earn points {points}</span>
+            points > 0 && <span className={`absolute z-10 ${offer.length > 0 ? 'top-10' : 'top-2'} right-2 bg-purple-900 px-3 rounded-xl font-bold text-white`}>Earn points {points}</span>
           }
 
           {/* Image section */}
           {
-            imgSrcUrl.length > 0 ? <Image className='w-full object-cover' src={imgSrcUrl} alt={altTextForImg} width={300}
+            imgSrcUrl.length > 0 ? <Image className='w-full object-cover group-hover:scale-110 transition-all duration-500' src={imgSrcUrl} alt={altTextForImg} width={300}
               height={300} priority /> : <Image className='w-full object-cover' src='/images/noImageFoundPotrait.jpg' alt='No image found Thumbnail Image' width={300} height={300} priority />
           }
 
           {/* status section */}
-          <span className={`absolute bottom-2 left-2 font-bold text-xl ${!imgSrcUrl.length > 0 ? 'text-black' : 'px-1 text-white bg-gray-700 bg-opacity-50'}`}>{status}</span>
+          <span className={`absolute z-10 bottom-2 left-2 font-bold text-xl ${!imgSrcUrl.length > 0 ? 'text-black' : 'px-1 text-white bg-gray-700 bg-opacity-50'}`}>{status}</span>
         </figure>
 
         <section className='flex-1 flex flex-col justify-between border'>
