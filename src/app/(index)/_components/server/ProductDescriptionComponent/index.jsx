f7dd0dd8fc84e3c2823b09666c53c20ab29eaba1
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-12-27 19:42:21 +0600

    some minor bugs fixed.

--- src/app/(index)/_components/server/ProductDescriptionComponent/index.jsx
+++ src/app/(index)/_components/server/ProductDescriptionComponent/index.jsx
@@ -9,11 +9,10 @@ function ProductDescriptionComponent({productDescriptionArr}) {
               productDescriptionArr.map(descriptionObj => <ul className='flex flex-col gap-2' key={`descParentUL${uuidv4()}`}>
                 {
                     Object.keys(descriptionObj).map(key => <li key={`descListItem${uuidv4()}`}>
-                        <span className='text-xl font-semibold capitalize'>{fromCamelCase(key)}:</span> <span className={`${key === 'title' && 'font-semibold text-lg capitalize'}`}>{key === 'title' ? fromCamelCase(descriptionObj[key]) : descriptionObj[key]}</span>
+                        <span className='text-xl font-semibold capitalize'>{fromCamelCase(key)}:</span> <span className={`${key === 'title' && 'font-semibold text-lg capitalize'}`}>{descriptionObj[key]}</span>
                     </li>)
                 }
             </ul>
-                
             )
         }
     </div>
