245419c0161428cf1b219a015a9a46537ae25794
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-12-27 18:49:45 +0600

    product specifications component added styled and modified.

--- src/app/(index)/_components/client/SingleProductLayout/index.jsx
+++ src/app/(index)/_components/client/SingleProductLayout/index.jsx
@@ -6,6 +6,7 @@ import { IoIosCart } from "react-icons/io";
 import { FaCartPlus } from "react-icons/fa";
 import KeyFeaturesComponent from '../../server/KeyFeaturesComponent';
 import BtnComponentOne from '../../server/BtnComponentOne';
+import ProductSpecificationComponent from '../../server/ProductSpecificationComponent';
 
 function SingleProductLayout({ id }) {
     const [productData, setProductData] = useState(null);
@@ -21,11 +22,14 @@ function SingleProductLayout({ id }) {
     }, [id]);
     return (
         <main className='w-auto md:mx-5 xl:w-2/3 xl:mx-auto' role='main'>
+            {/* product title */}
             {
                 serverResMsg && <h5 className='my-5 text-lg text-center font-bold'>{serverResMsg}</h5>
             }
+
+            {/* key features section */}
             {
-                !serverResMsg && productData && <section className='flex flex-col lg:flex-row gap-5'>
+                !serverResMsg && productData?.keyFeatures && <section className='flex flex-col lg:flex-row gap-5'>
                     {/* image section component */}
                     <SingleProductImgCarousel className='lg:w-1/2' imgSrcArr={productData?.imgUrls} />
 
@@ -35,12 +39,12 @@ function SingleProductLayout({ id }) {
                         <h2 className='font-semibold text-2xl my-5'>{productData?.productTitle}</h2>
 
                         {
-                            productData.points && <p className='my-5 text-xl'><span className='font-semibold'>Earn:</span> {productData.points} points</p>
+                            productData?.points && <p className='my-5 text-xl'><span className='font-semibold'>Earn:</span> {productData.points} points</p>
                         }
 
                         {/* key features of the product display section */}
                         {
-                            Object.keys(productData?.keyFeatures).length > 0 && <KeyFeaturesComponent keyFeaturesObj={productData?.keyFeatures} price={productData.price} regularPrice={productData.regularPrice} />
+                            Object.keys(productData.keyFeatures).length > 0 && <KeyFeaturesComponent keyFeaturesObj={productData.keyFeatures} price={productData?.price} regularPrice={productData?.regularPrice} />
                         }
 
                         {/* Buttons section for several actions */}
@@ -49,6 +53,16 @@ function SingleProductLayout({ id }) {
                             <BtnComponentOne iconFirst={<FaCartPlus />} className='bg-[#ef4444] pe-8' >Add To Cart</BtnComponentOne>
                         </section>
                     </section>
+                </section>
+            }
+
+            {/* product specification details section */}
+            {
+                !serverResMsg && productData?.productSpecifications.length > 0 && <section className='mt-24'>
+                    <h2 className='font-semibold text-2xl mb-5 text-center'>Product&apos;s Specification details down bellow</h2>
+
+                    {/* product's specificaiton component */}
+                    <ProductSpecificationComponent ProductSpecificationsArr={productData.productSpecifications} />
                 </section>
             }
         </main>
---
+++ src/app/(index)/_components/server/ProductSpecificationComponent/index.jsx
@@ -0,0 +1,40 @@
+import { fromCamelCase } from '@/utils';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+function ProductSpecificationComponent({ ProductSpecificationsArr }) {
+    // returing html table structure as jsx elements nested inside a sigle div element.
+    return (
+        <div>
+            {
+                // iterating through props array
+                ProductSpecificationsArr.map(productSpecsObj => <section className='mx-5 my-12 md:mx-auto' key={uuidv4()}>
+                    {
+                        // iterating through objects of that array
+                        Object.keys(productSpecsObj).map(keyObjTitle => <table className='table-auto flex flex-col gap-3 dark:gap-2' key={`individualSpecs${uuidv4()}`}>
+                            {/* iterating through nested of objects for each object obtained from ProductSpecificationsArr(array received as props) */}
+                            {
+                                <>
+                                    <thead className='bg-gray-800 text-white dark:bg-[#2a2424] px-2 py-1 xl:px-8'>
+                                        <tr className='text-lg font-semibold capitalize mt-8'><td colSpan="2">{fromCamelCase(keyObjTitle)}:</td></tr>
+                                    </thead>
+
+                                    {/* iterating through nested object of those retrieved objects for each object */}
+                                    <tbody className='flex flex-col gap-1'>
+                                        {
+                                            Object.keys(productSpecsObj[keyObjTitle]).map((key, index) => <tr className={`text-md py-1 ${index % 2 === 0 ? 'bg-[#f0f6fb] dark:bg-[#585657]' : 'dark:bg-[#2c2b2b]'}`} key={`individualSpecListItem${uuidv4()}`}>
+                                                <td className='capitalize font-semibold leading-5 ps-2 xl:ps-16'>{fromCamelCase(key)}:</td><td className='px-2'>{productSpecsObj[keyObjTitle][key]}</td>
+                                            </tr>)
+                                        }
+                                    </tbody>
+                                </>
+                            }
+                        </table>)
+                    }
+                </section>)
+            }
+        </div>
+    )
+}
+
+export default ProductSpecificationComponent;
