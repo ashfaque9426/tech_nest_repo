f7f8548d7a315295ffcae57d543ed24af8782c18
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2024-01-08 18:56:05 +0600

    product specification section updated.

--- src/app/(index)/_components/server/ProductSpecificationComponent/index.jsx
+++ src/app/(index)/_components/server/ProductSpecificationComponent/index.jsx
@@ -8,22 +8,37 @@ function ProductSpecificationComponent({ ProductSpecificationsArr }) {
         <div>
             {
                 // iterating through props array
-                ProductSpecificationsArr.map(productSpecsObj => <section className='mb-12 md:my-12' key={uuidv4()}>
+                ProductSpecificationsArr.map(productSpecsObj => <section role='region' className='mb-12 md:my-12' key={uuidv4()}>
                     {
                         // iterating through objects of that array
                         Object.keys(productSpecsObj).map(keyObjTitle => <table className='table-auto flex flex-col gap-3 dark:gap-2' key={`individualSpecs${uuidv4()}`}>
                             {/* iterating through nested of objects for each object obtained from ProductSpecificationsArr(array received as props) */}
                             {
                                 <>
-                                    <thead className='bg-gray-800 text-white dark:bg-[#2a2424] p-2 xl:px-8'>
+                                    <thead className='bg-gray-800 text-white dark:bg-[#2a2424] ps-3 py-2 xl:ps-8'>
                                         <tr className='text-lg font-semibold capitalize mt-8'><td colSpan="2">{fromCamelCase(keyObjTitle)}:</td></tr>
                                     </thead>
 
                                     {/* iterating through nested object of those retrieved objects for each object */}
                                     <tbody className='flex flex-col gap-1'>
                                         {
-                                            Object.keys(productSpecsObj[keyObjTitle]).map((key, index) => <tr className={`text-md py-2 ${index % 2 === 0 ? 'bg-[#f0f6fb] dark:bg-[#585657]' : 'bg-[#fbfbfb] dark:bg-[#2c2b2b]'}`} key={`individualSpecListItem${uuidv4()}`}>
-                                                <td className='capitalize font-semibold leading-5 ps-2 xl:ps-14'>{fromCamelCase(key)}:</td><td className='px-2'>{productSpecsObj[keyObjTitle][key]}</td>
+                                            Object.keys(productSpecsObj[keyObjTitle]).map((key, index) => <tr className={`text-md py-2 flex ${index % 2 === 0 ? 'bg-[#f0f6fb] dark:bg-[#585657]' : 'bg-[#fbfbfb] dark:bg-[#2c2b2b]'}`} key={`individualSpecListItem${uuidv4()}`}>
+                                                <td className='capitalize font-semibold leading-5 ps-3 xl:ps-8 w-1/3 flex items-center'>{fromCamelCase(key)}:</td><td className='px-3 w-2/3'>
+                                                    {
+                                                        productSpecsObj[keyObjTitle][key].includes(";") ? 
+                                                            <ul>
+                                                                {
+                                                                    productSpecsObj[keyObjTitle][key].split("; ").map((str, i) => (
+                                                                        <li className={`${i !== productSpecsObj[keyObjTitle][key].split("; ").length - 1 ? 'mb-2' : 'mb-0'}`} key={`strOfArr${uuidv4()}`}>
+                                                                            {str}
+                                                                        </li>
+                                                                    ))
+                                                                }
+                                                            </ul>
+                                                        :
+                                                            productSpecsObj[keyObjTitle][key]
+                                                    }
+                                                </td>
                                             </tr>)
                                         }
                                     </tbody>
