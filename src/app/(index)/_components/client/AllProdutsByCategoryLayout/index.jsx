b79247cda97a1ab572aa43cbd589f8455824a33b
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2024-01-15 22:35:15 +0600

    sidebar components added for destop processors and so do sorting functionality implemented according to product price.

--- src/app/(index)/_components/client/AllProdutsByCategoryLayout/index.jsx
+++ src/app/(index)/_components/client/AllProdutsByCategoryLayout/index.jsx
@@ -7,12 +7,23 @@ import { v4 as uuidv4 } from 'uuid';
 
 function AllProductsByCategoryLayout({ category }) {
     const [productArr, setProductArr] = useState([]);
+    const [sortOrder, setSortOrder] = useState('lowToHigh');
+    const [processorSidebar, setProcessorSidebar] = useState([{ brandOne: { brand: "Intel", models: ['Intel Pentium', 'Core i3', 'Core i5', 'Core i7', 'Core i9'], socketList: ['LGA1700', 'FCLGA1200', 'LGA2011'] } }, { brandTwo: { brand: "AMD", models: ['Ryzen 3', 'Ryzen 5', 'Ryzen 7', 'Ryzen Threadripper'], socketList: ['AM5', 'sWRX8'] } }]);
+
+    // for changing sort order state
+    const handleSortOrderChange = e => {
+        setSortOrder(e.target.value);
+    }
 
     useEffect(() => {
         productByCategory(category).then(result => {
             if (result.success) {
-                setProductArr(result.data);
+                // sorting data according to sort product and loading data on page load.
+                setProductArr(sortOrder === 'lowToHigh' && result.data || sortOrder === 'highToLow' && result.data.sort(function (a, b) {
+                    return b.regularPrice - a.regularPrice;
+                }));
             }else {
+                // show error if unable to get products for some reason.
                 toast.error(result.message, {
                     position: "top-right",
                     autoClose: 5000,
@@ -27,18 +38,71 @@ function AllProductsByCategoryLayout({ category }) {
             }
         }).catch(err => console.log(err.message));
         
-    }, [category]);
+    }, [category, sortOrder]);
     return (
         <>
-            <aside className='md:w-[25%] border' role="complementary" aria-labelledby="sidebar-heading">
-                <h2 id="sidebar-heading">Sidebar Content</h2>
+            <aside className='md:w-[25%] md:sticky md:top-0' role="complementary" aria-labelledby="sidebar-heading">
+                <h2 className='text-xl font-semibold text-center' id="sidebar-heading">Filtering Options</h2>
+
+                {/* for destop processors side menubar filtering options */}
+                {
+                    category === "desktop processor" || category === "desktop%20processor" && <div className='flex flex-col gap-5 items-center my-5'>
+                        {
+                            processorSidebar.map(sidebarComponentObj => (
+                                Object.keys(sidebarComponentObj).map(keyObj => (<section className='w-full  rounded-lg shadow-md bg-[#efeded] dark:bg-[#1e1e1e] dark:shadow-none py-5' key={`processorFilteringItems${uuidv4()}`}>
+                                    {   
+                                        <div className='w-2/3 mx-auto'>
+                                            <h3 className='text-lg capitalize font-semibold mb-3'>{sidebarComponentObj[keyObj].brand}</h3>
+                                            <ul className='my-2'>
+                                                <h4 className='mb-1 font-semibold'>Models:</h4>
+                                                {
+                                                    sidebarComponentObj[keyObj].models.map(strItem => (
+                                                        <li className='hover:cursor-pointer flex items-center gap-1' key={`processorFilteringByModels${uuidv4()}`}>
+                                                            <span>
+                                                                <form>
+                                                                    <input type="checkbox" name="checkbox" />
+                                                                </form>
+                                                            </span>
+                                                            <span>{strItem}</span>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+
+                                            <ul className='my-2'>
+                                                <h4 className='mb-1 font-semibold'>Sockets:</h4>
+                                                {
+                                                    sidebarComponentObj[keyObj].socketList.map(strItem => (
+                                                        <li className='hover:cursor-pointer flex items-center gap-1' key={`processorFilteringBySocketList${uuidv4()}`}>
+                                                            <span>
+                                                                <form>
+                                                                    <input type="checkbox" name="checkbox" />
+                                                                </form>
+                                                            </span>
+                                                            <span>{strItem}</span>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                    }
+                                </section>))
+                            ))
+                        }
+                    </div>
+                }
             </aside>
 
+            {/* Product display section */}
             <section className='md:w-[75%]' role='region' aria-labelledby="section-heading">
-                <h2 className='text-lg md:text-2xl font-bold mb-5' id="section-heading">{productArr?.length > 0 && productArr[0].productCategory + 's:'}</h2>
+                <h2 className='mb-5 flex justify-between items-center' id="section-heading"><span className='text-lg md:text-2xl font-bold'>{productArr?.length > 0 && productArr[0].productCategory + 's:'}</span> <span><label className='text-lg font-semibold mr-2' htmlFor="sortOrder">Sort By Price:</label>
+                    <select onChange={handleSortOrderChange} id="sortOrder" name="sortOrder" className="p-2 border border-gray-300 rounded cursor-pointer focus:border-gray-500 dark:bg-[#1e1e1e]">
+                        <option value="lowToHigh">Low to High</option>
+                        <option value="highToLow">High to Low</option>
+                    </select></span></h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 5xl:grid-cols-4 gap-5'>
                     {
-                        productArr?.length > 0 && productArr.map(productObj => <CardComponentTwo key={`cardCompTwoKey${uuidv4()}`} productObj={productObj} />)
+                        productArr?.length > 0 && productArr.map(productObj => <CardComponentTwo className='bg-[#fcfcfc]' key={`cardCompTwoKey${uuidv4()}`} productObj={productObj} />)
                     }
                 </div>
             </section>
--- src/app/(index)/_components/shared/server/CardComponentTwo/index.jsx
+++ src/app/(index)/_components/shared/server/CardComponentTwo/index.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import Image from 'next/image';
 import { v4 as uuidv4 } from 'uuid';
-import { calculateDiscountPercentage, formatNumberWithCommas, fromCamelCase } from '@/utils';
+import { formatNumberWithCommas, fromCamelCase } from '@/utils';
 import ShowMoreBtn from '../ShowMoreBtn';
 import cn from '@/lib/clsx/cn';
 
-function CardComponentTwo({ productObj, className, classNameForSection, classNameForProductTitle, classNameForKeyFeatureContainer, classNameForKeyFeatureKeys, classNameForSavePercentageSection, classNameForBrandSection }) {
+function CardComponentTwo({ productObj, className, classNameForSection, classNameForSectionDiv, classNameForProductTitle, classNameForKeyFeatureContainer, classNameForKeyFeatureKeys, classNameForSavePercentageSection, classNameForBrandSection }) {
     const { _id, brand, productTitle, imgUrls, keyFeatures, regularPrice, price } = productObj;
     return (
-        <article className={cn('h-full flex flex-col overflow-clip rounded-lg shadow-md hover:shadow-lg dark:shadow-none dark:bg-[#1e1e1e]', className)}>
+        <article className={cn('h-full pb-2 flex flex-col overflow-clip rounded-lg shadow-md hover:shadow-lg dark:shadow-none dark:bg-[#1e1e1e]', className)}>
             <figure className='overflow-clip relative border rounded-tl-lg rounded-tr-lg dark:border-none'>
-                <small className={cn('absolute top-5 px-3 text-white font-bold bg-red-500 rounded-e-lg', classNameForSavePercentageSection)}>{calculateDiscountPercentage(regularPrice, price) > 0 && `Save ${calculateDiscountPercentage(regularPrice, price).toFixed(2)}%` }</small>
+                <small className={cn('absolute top-5 px-3 text-white font-bold bg-red-500 rounded-e-lg', classNameForSavePercentageSection)}>{`Save ${formatNumberWithCommas(regularPrice - price)} BDT` }</small>
                 <small className={cn('absolute px-3 py-1 bottom-5 right-5 bg-[#3d393f]  text-white font-bold rounded-lg', classNameForBrandSection)}>{brand}</small>
                 <small className={cn('absolute px-3 py-1 bottom-5 bg-[#3d393f]  text-white font-bold rounded-e-lg', classNameForBrandSection)}>{formatNumberWithCommas(regularPrice)} BDT</small>
                 <Image className='object-cover w-full h-full' src={imgUrls[0]} alt={`${productTitle} card image`} width={550} height={550} priority />
             </figure>
-            <section className={cn('p-2 flex-1 flex flex-col justify-between gap-3', classNameForSection)}>
-                <h3 className={cn('font-semibold', classNameForProductTitle)}>{productTitle}</h3>
-                <ul className={cn('text-sm flex flex-col gap-1', classNameForKeyFeatureContainer)}>
-                    {
-                        Object.keys(keyFeatures).map(key => <li key={`featureListItem${uuidv4()}`}>
-                            <span className={cn('capitalize font-semibold', classNameForKeyFeatureKeys)}>{fromCamelCase(key)}:</span>
-                            <span className='ms-1'>{keyFeatures[key]}</span>
-                        </li>)
-                    }
-                </ul>
+            <section className={cn('p-2 flex-1 flex flex-col justify-between gap-5', classNameForSection)} role='region'>
+                <div className={cn('flex flex-col gap-3', classNameForSectionDiv)}>
+                    <h3 className={cn('font-semibold', classNameForProductTitle)}>{productTitle}</h3>
+                    <ul className={cn('text-sm flex flex-col gap-1', classNameForKeyFeatureContainer)}>
+                        {
+                            Object.keys(keyFeatures).map(key => <li key={`featureListItem${uuidv4()}`}>
+                                <span className={cn('capitalize font-semibold', classNameForKeyFeatureKeys)}>{fromCamelCase(key)}:</span>
+                                <span className='ms-1'>{keyFeatures[key]}</span>
+                            </li>)
+                        }
+                    </ul>
+                </div>
                 <ShowMoreBtn className='no-underline font-semibold px-2 py-1 bg-red-500 hover:bg-red-600 rounded-lg text-white' url={`/singleProduct/${_id}`}>View Details</ShowMoreBtn>
             </section>
         </article>
