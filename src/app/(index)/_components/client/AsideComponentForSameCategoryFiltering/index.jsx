5d0976795ec6198e9d6c23cce64556ea5d7d7e4e
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2024-01-29 13:59:49 +0600

    some modifications made for array of elements of filtering options for so that dynamic element as filteration option can be added in the future.

--- src/app/(index)/_components/client/AsideComponentForSameCategoryFiltering/index.jsx
+++ src/app/(index)/_components/client/AsideComponentForSameCategoryFiltering/index.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import LiComponentForAsideCompFiltering from '../LiComponentForAsideCompFiltering';
+import { fromCamelCase } from '@/utils';
 
 function AsideComponentForSameCategoryFiltering({ category, brand, handleParamsForUrl }) {
     // initializing states
@@ -25,28 +26,29 @@ function AsideComponentForSameCategoryFiltering({ category, brand, handleParamsF
             {
                 category === "desktop processor" || category === "desktop%20processor" && <div className='flex flex-col gap-5 items-center my-5'>
                     {
+                        // given state here will be always an array.
                         processorSidebar.map(sidebarComponentObj => (
                             Object.keys(sidebarComponentObj).map(keyObj => (<section className='w-full  rounded-lg shadow-md bg-[#efeded] dark:bg-[#1e1e1e] dark:shadow-none py-8' key={`processorFilteringItems${uuidv4()}`}>
                                 {
                                     <div className='w-2/3 3xl:w-[45%] mx-auto'>
-                                        <h3 className='text-lg capitalize font-semibold mb-3'><span className={`${sidebarComponentObj[keyObj].brand === 'Intel' && 'px-3 py-1 rounded-xl bg-[#006bae]' || sidebarComponentObj[keyObj].brand === 'AMD' && 'px-3 py-1 rounded-xl bg-[#c94c2e]'}`}>{sidebarComponentObj[keyObj].brand}</span></h3>
-                                        <ul className='my-2 3xl:my-5'>
-                                            <h4 className='mb-1 font-semibold'>Models:</h4>
-                                            {
-                                                sidebarComponentObj[keyObj].models.map((strItem, i) => (
-                                                    <LiComponentForAsideCompFiltering key={`processorFilteringByModels${uuidv4()}`} strItem={strItem} i={i} handleParamsForUrl={handleParamsForUrl} indexArr={indexArr} setIndexArr={setIndexArr} />
-                                                ))
-                                            }
-                                        </ul>
+                                        {/* for main title such as brand name. */}
+                                        <h3 className='text-lg capitalize font-semibold mb-3'><span className={`${sidebarComponentObj[keyObj].brand === 'Intel' && 'px-3 py-1 rounded-xl bg-[#006bae]' || sidebarComponentObj[keyObj].brand === 'AMD' && 'px-3 py-1 rounded-xl bg-[#c94c2e]'}`}>{sidebarComponentObj[keyObj][Object.keys(sidebarComponentObj[keyObj])[0]]}</span></h3>
 
-                                        <ul className='my-2'>
-                                            <h4 className='mb-1 font-semibold'>Sockets:</h4>
-                                            {
-                                                sidebarComponentObj[keyObj].socketList.map((strItem, i) => (
-                                                    <LiComponentForAsideCompFiltering key={`processorFilteringByModels${uuidv4()}`} strItem={strItem} i={i} handleParamsForUrl={handleParamsForUrl} indexArr={indexArr} setIndexArr={setIndexArr} />
-                                                ))
-                                            }
-                                        </ul>
+                                        {
+                                            Object.keys(sidebarComponentObj[keyObj]).map(indexStr => (
+                                                Array.isArray(sidebarComponentObj[keyObj][indexStr]) && <ul className='my-2 3xl:my-5' key={`processFilteringUnorderedList${uuidv4()}`}>
+                                                    {/* for sub main title such as what is a list of item about in this ul */}
+                                                    <h4 key={`headingElem${uuidv4()}`} className='mb-1 font-semibold capitalize'>{fromCamelCase(indexStr)}</h4>
+
+                                                    {/* iterate through the array from the object of given state and return li component. */}
+                                                    {
+                                                        sidebarComponentObj[keyObj][indexStr].map((strItem, i) => (
+                                                            <LiComponentForAsideCompFiltering key={`processorFilteringByModels${uuidv4()}`} strItem={strItem} i={i} handleParamsForUrl={handleParamsForUrl} indexArr={indexArr} setIndexArr={setIndexArr} />
+                                                        ))
+                                                    }
+                                                </ul>
+                                            ))
+                                        }
                                     </div>
                                 }
                             </section>))
