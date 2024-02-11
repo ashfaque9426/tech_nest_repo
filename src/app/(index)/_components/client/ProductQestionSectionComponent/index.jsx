b6b892975b8fa6c6189c1db44ed325b9f2d31752
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2024-02-11 17:21:51 +0600

    Single product compoenent is up for server side rendering now.

---
+++ src/app/(index)/_components/client/ProductQestionSectionComponent/index.jsx
@@ -0,0 +1,103 @@
+"use client"
+import React, { useState } from 'react';
+import { addQuestionForProduct } from '@/services/productServices';
+import { v4 as uuidv4 } from 'uuid';
+import { toast } from 'react-toastify';
+import BtnComponentOne from '../../server/BtnComponentOne';
+import UsersQuestionsComponent from '../UsersQuestionsComponent';
+
+function ProductQestionSectionComponent({ id, serverResMsg, productData }) {
+    const [viewQuestionFrom, setViewQuestionForm] = useState(false);
+    const [questionsArr, setQuestionsArr] = useState([]);
+
+    const handleQuestionSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const userName = form.userName.value;
+        const userEmail = form.userEmail.value;
+        const usersQuestion = form.usersQuestion.value;
+
+        const questionObj = {
+            qid: `questionID${uuidv4()}`,
+            userName: userName.length > 0 ? userName : 'Unknown User',
+            userEmail: userEmail,
+            usersQuestions: [{ question: usersQuestion, ansswer: '' }]
+        }
+
+        addQuestionForProduct(id, questionObj, userEmail).then(data => {
+            if (data.success) {
+                // refetching the data again on successfull question added to the server.
+                setQuestionsArr(data.updatedDataArr);
+
+                toast(data.message, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            } else {
+                toast(data.message, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            }
+        }).catch(err => console.log(err.message));
+
+        // console.log(questionObj);
+        // resetting form data
+        form.reset();
+        setViewQuestionForm(false);
+    }
+
+    return (
+        <section className='mt-24 mx-5 2xl:mx-0' role="region" aria-labelledby="section4Label">
+            {
+                !serverResMsg && productData?.questions.length > 0 && <div>
+                    <h2 id='section4LabelHeading' className='font-semibold text-[22px] md:text-2xl mb-8 md:text-center'>Questions related to {productData?.productTitle}.</h2>
+
+                    <UsersQuestionsComponent usersQuestionsArr={questionsArr.length > 0 ? questionsArr : productData.questions} />
+                </div>
+            }
+
+
+            <div className='mt-12'>
+                <form className={viewQuestionFrom ? 'block w-auto md:w-2/3 md:mx-auto' : 'hidden'} onSubmit={handleQuestionSubmit}>
+                    <fieldset className='border border-black dark:border-white shadow-lg dark:shadow-none rounded-md p-5 flex flex-col gap-3'>
+                        <legend className='font-bold'>User Question From</legend>
+                        <section className='flex flex-col gap-1'>
+                            <label className='font-semibold' htmlFor="userName">Provide User Name</label>
+                            <input className='border border-black dark:text-black dark:border-white px-3 py-1 rounded-md' type="text" name="userName" id="userName" placeholder="Your Name(Optional)" autoComplete="username" />
+                        </section>
+
+                        <section className='flex flex-col gap-1'>
+                            <label className='font-semibold' htmlFor="userEmail">Your Email*</label>
+                            <input className='border border-black dark:text-black dark:border-white px-3 py-1 rounded-md' type="email" name="userEmail" id="userEmail" placeholder="Your Email" required autoComplete="email" />
+                        </section>
+
+                        <section className='flex flex-col gap-1'>
+                            <label className='font-semibold' htmlFor="usersQuestion">Your Question*</label>
+                            <textarea className='border border-black dark:text-black dark:border-white px-3 py-1 rounded-md' name="usersQuestion" id="usersQuestion" cols="10" rows="5" placeholder="Provide Your Question" required ></textarea>
+                        </section>
+                    </fieldset>
+
+                    <input className='px-3 py-1 text-white bg-[#585657] rounded-lg my-3' type="submit" value="Submit" />
+                </form>
+                <div className={!viewQuestionFrom ? 'flex justify-center' : 'hidden'}>
+                    <BtnComponentOne onClickFunc={() => setViewQuestionForm(true)}>Add a Question?</BtnComponentOne>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProductQestionSectionComponent;
--- src/app/(index)/_components/client/SingleProductLayout/index.jsx
+++ src/app/(index)/_components/client/SingleProductLayout/index.jsx
@@ -1,6 +1,5 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { addQuestionForProduct, productById } from '@/services/productServices';
+// import React, { useEffect, useState } from 'react';
+import { productById } from '@/services/productServices';
 import SingleProductImgCarousel from '../SingleProductImgCarousel';
 import { IoIosCart } from "react-icons/io";
 import { FaCartPlus } from "react-icons/fa";
@@ -8,73 +7,10 @@ import KeyFeaturesComponent from '../../shared/server/KeyFeaturesComponent';
 import BtnComponentOne from '../../server/BtnComponentOne';
 import ProductSpecificationComponent from '../../server/ProductSpecificationComponent';
 import ProductDescriptionComponent from '../../server/ProductDescriptionComponent';
-import { v4 as uuidv4 } from 'uuid';
-import { toast } from 'react-toastify';
-import UsersQuestionsComponent from '../UsersQuestionsComponent';
+import ProductQestionSectionComponent from '../ProductQestionSectionComponent';
 
-function SingleProductLayout({ id }) {
-    const [productData, setProductData] = useState(null);
-    const [serverResMsg, setServerResMsg] = useState(null);
-    const [viewQuestionFrom, setViewQuestionForm] = useState(false);
-    const [questionsArr, setQuestionsArr] = useState([]);
-
-    const handleQuestionSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        const userName = form.userName.value;
-        const userEmail = form.userEmail.value;
-        const usersQuestion = form.usersQuestion.value;
-
-        const questionObj = {
-            qid: `questionID${uuidv4()}`,
-            userName: userName.length > 0 ? userName : 'Unknown User',
-            userEmail: userEmail,
-            usersQuestions: [{ question: usersQuestion, ansswer: '' }]
-        }
-
-        addQuestionForProduct(id, questionObj, userEmail).then(data => {
-            if(data.success) {
-                // refetching the data again on successfull question added to the server.
-                setQuestionsArr(data.updatedDataArr);
-
-                toast(data.message, {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-            } else {
-                toast(data.message, {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-            }
-        }).catch(err => console.log(err.message));
-
-        // console.log(questionObj);
-        // resetting form data
-        form.reset();
-        setViewQuestionForm(false);
-    }
-
-    useEffect(() => {
-        // setting up the retrived data from server to setProduct data route.
-        productById(id).then(result => {
-            setProductData(result.data);
-            setServerResMsg(result?.message);
-            // console.log(result.data)
-        });
-    }, [id]);
+async function SingleProductLayout({ id }) {
+    const { success, data: productData, message: serverResMsg } = await productById(id);
     
     return (
         <main role='main' aria-labelledby='subMainContentLabel'>
@@ -140,44 +76,8 @@ function SingleProductLayout({ id }) {
             }
 
             {/* product questions section */}
-            <section className='mt-24 mx-5 2xl:mx-0' role="region" aria-labelledby="section4Label">
-                {
-                    !serverResMsg && productData?.questions.length > 0 && <div>
-                        <h2 id='section4LabelHeading' className='font-semibold text-[22px] md:text-2xl mb-8 md:text-center'>Questions related to {productData?.productTitle}.</h2>
-
-                        {/* users questions component to view questions lists */}
-                        <UsersQuestionsComponent usersQuestionsArr={questionsArr.length > 0 ? questionsArr : productData.questions} />
-                    </div>
-                }
-
-                {/* User's Question Field */}
-                <div className='mt-12'>
-                    <form className={viewQuestionFrom ? 'block w-auto md:w-2/3 md:mx-auto':'hidden'} onSubmit={handleQuestionSubmit}>
-                        <fieldset className='border border-black dark:border-white shadow-lg dark:shadow-none rounded-md p-5 flex flex-col gap-3'>
-                            <legend className='font-bold'>User Question From</legend>
-                            <section className='flex flex-col gap-1'>
-                                <label className='font-semibold' htmlFor="userName">Provide User Name</label>
-                                <input className='border border-black dark:text-black dark:border-white px-3 py-1 rounded-md' type="text" name="userName" id="userName" placeholder="Your Name(Optional)" autoComplete="username" />
-                            </section>
-
-                            <section className='flex flex-col gap-1'>
-                                <label className='font-semibold' htmlFor="userEmail">Your Email*</label>
-                                <input className='border border-black dark:text-black dark:border-white px-3 py-1 rounded-md' type="email" name="userEmail" id="userEmail" placeholder="Your Email" required autoComplete="email" />
-                            </section>
-
-                            <section className='flex flex-col gap-1'>
-                                <label className='font-semibold' htmlFor="usersQuestion">Your Question*</label>
-                                <textarea className='border border-black dark:text-black dark:border-white px-3 py-1 rounded-md' name="usersQuestion" id="usersQuestion" cols="10" rows="5" placeholder="Provide Your Question" required ></textarea>
-                            </section>
-                        </fieldset>
-
-                        <input className='px-3 py-1 text-white bg-[#585657] rounded-lg my-3' type="submit" value="Submit" />
-                    </form>
-                    <div className={!viewQuestionFrom ? 'flex justify-center' : 'hidden'}>
-                        <BtnComponentOne onClickFunc={() => setViewQuestionForm(true)}>Add a Question?</BtnComponentOne>
-                    </div>
-                </div>
-            </section>
+            <ProductQestionSectionComponent id={id} productData={productData} serverResMsg={serverResMsg} />
+            
         </main>
     )
 }
